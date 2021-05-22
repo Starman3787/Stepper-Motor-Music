93b949009d34d608f8c43ec9f1cfc57d150d6f43
Starman <30315137+Starman3787@users.noreply.github.com>
2021-05-22 17:37:00 +0100

    change microseconds to uint64_t

--- systick.c
+++ systick.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
-uint32_t microseconds = 0;
+uint64_t microseconds = 0;
 uint32_t milliseconds = 0;
 
-uint32_t currentMicroseconds(void)
+uint64_t currentMicroseconds(void)
 {
     return microseconds;
 }
